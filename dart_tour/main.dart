a58be30af758c26d7e6167f02a84d9ef741b95a0
alsdl0629 <ehfehf0629@gmail.com>
2024-01-02 22:42:26 +0900

    변수 선언

--- dart_tour/main.dart
+++ dart_tour/main.dart
@@ -1,3 +1,31 @@
 void main() {
   print("hello");
+
+  // 컴파일시 타입 지정
+  var name = "강민";
+
+  // 타입 명시
+  String name1 = "강민";
+
+  var a;
+  dynamic b;
+  a = 1;
+  a = "강민";
+  a = true;
+
+  // null safety
+  String? c = "강민";
+  c = null;
+
+  if (c != null) {
+    c.isNotEmpty;
+  }
+
+  c?.isNotEmpty;
+
+  final d = "강민";
+  // d = "강민1";
+
+  final e;
+  e = "강민";
 }
